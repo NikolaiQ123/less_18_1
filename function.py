641a78b727e49e147fed70b72707b25cafca8471
Nikolai <blichnaruto@gmail.com>
2023-12-21 19:47:37 +0300

    create func sub

--- function.py
+++ function.py
@@ -9,3 +9,17 @@ def add(a:int, b:int) -> int:
     '''
 
     return a + b
+
+
+
+def sub(a:int, b:int) -> int:
+    '''
+
+    add a + b
+
+    :param a:
+    :param b:
+    :return: result
+    '''
+
+    return a - b
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from function import add
+from function import add, sub
 
 if __name__ == '__main__':
 
+    # assert function add
     assert add(1, 1) == 2
     assert add(1, 2) == 3
     assert add(1, 3) == 4
 
-    a, b = map(int, input(': ').split())
+    # assert function sub
+    assert sub(2, 1) == 1
+    assert sub(3, 1) == 2
+    assert sub(4, 1) == 3
+
+
+    a, b = map(int, input('a & b: ').split())
+
     print(f'result: {add(a, b)}')
+    print(f'result: {sub(a, b)}')
