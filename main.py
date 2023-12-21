fc122b54b5bef735237874752b0e660c7231597a
Nikolai <blichnaruto@gmail.com>
2023-12-21 19:29:32 +0300

    create main structure

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print('Hello worldgit ')
+
+def add(a:int, b:int) -> int:
+    '''
+
+    add a + b
+
+    :param a:
+    :param b:
+    :return: result
+    '''
+
+    return a + b
+
+
+
+if __name__ == '__main__':
+
+    assert add(1, 1) == 2
+    assert add(1, 2) == 3
+    assert add(1, 3) == 4
+
+    a, b = map(int, input(': ').split())
+    print(f'result {add(a, b)}')
+
+
